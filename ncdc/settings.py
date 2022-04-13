42cc32649d8fa11127c9e39aadd1fe0d96dac065
salamkayodeji <salamkayodeji@gmail.com>
2022-04-13 21:21:54 +0100

    cors edit

--- ncdc/settings.py
+++ ncdc/settings.py
@@ -40,21 +40,20 @@ INSTALLED_APPS = [
     'ncdcapi',
     'rest_framework',
     'corsheaders',
-]
+ ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
-
-CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
-CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_ALLOW_ALL = True
+#CORS_ALLOW_CREDENTIALS = True
 
 
 ROOT_URLCONF = 'ncdc.urls'
