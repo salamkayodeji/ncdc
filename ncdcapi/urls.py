19bf636eb5b53a39d750085b56eb4e5c5f3bf329
salamkayodeji <salamkayodeji@gmail.com>
2022-04-13 19:27:43 +0100

    Added my project

--- ncdcapi/urls.py
+++ ncdcapi/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path, include
 from ncdcapi import views 
 from rest_framework import routers
+
 from rest_framework.routers import DefaultRouter
 from rest_framework import viewsets
+from rest_framework.schemas import get_schema_view
+from django.views.generic import TemplateView
 
 
 router = routers.DefaultRouter()
@@ -14,4 +17,14 @@ router.register(r'lga', views.LgaViewSet, 'lga')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('openapi/', get_schema_view(
+        title="Invoice Service",
+        description="API developers hoping to use our service"
+    ), name='openapi-schema'),
+    path('docs/', TemplateView.as_view(
+        template_name='ncdcapi/documentation.html',
+        extra_context={'schema_url':'openapi-schema'}
+    ), name='swagger-ui'),
+
+
 ]
